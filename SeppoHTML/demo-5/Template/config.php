7f8e898e5a047e2bc99b1cb2dc142957afb29cd3
amirestakhri <amirestakhri2004@gmail.com>
2024-11-08 05:10:11 +0330

    12th

--- SeppoHTML/demo-5/Template/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//SITE GLOBAL CONFIGURATION
-$email = "yourmail@here.com";  //<-- Your email where you will receive the emails from the new Subcribares
-$emailSubject = "New Subscriber from Seppo";
-$emailText = "New Subscriber: ";
-$successfulAlert = "Thank you for subscribing!";
-?>
